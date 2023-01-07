0a5ee81f791572b9bcd9b97c6087fb5b149c1e51
Mukul Gupta <mukulgupta257@gmail.com>
2023-01-07 17:25:39 +0530

    Updated Code .

--- money-claim/pages/profile.js
+++ money-claim/pages/profile.js
@@ -1,18 +1,21 @@
 import Cookies from "js-cookie";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { Switch } from "antd";
 import Layout from "../Components/Layout";
 import Data from "../JSON/home.json";
 
-export default function Home() {
+export default function Profile() {
   const [data, setData] = useState(0);
   const [login, setLogin] = useState(false);
+  const [user, setUser] = useState("");
   const router = useRouter();
   useEffect(() => {
     let userData = Cookies.get("UserInfo");
     userData = userData && JSON.parse(userData);
     if (userData && userData.name !== "") {
       console.log(userData);
+      setUser(userData);
       setLogin(true);
     } else {
       router.push("/");
@@ -31,16 +34,19 @@ export default function Home() {
     login && (
       <Layout>
         <div className="LoginBtnContainer">
-          <button onClick={handleClick} className={"LanguageButton"}>
-            Change Language / भाषा बदलें
-          </button>
+          <span>Change Language / भाषा बदलें</span>
+          <Switch onChange={handleClick} />
         </div>
-        <div className="ProfileHeader">
-          <span>Home Page</span>
+        <div className="ProfileHeader" onClick={() => router.push("/home")}>
+          {`🔙    Back`}
+        </div>
+        <div className="ProfileHeader2">
+          <span>Profile Page</span>
           <button onClick={handleLogout}>Logout</button>
         </div>
         <div className="btnContainer">
-          <div className="Clickbtn" onClick={() => router.push("/profile")}>
+          <div>{`Hello ${user.name}`}</div>
+          <div className="Clickbtn">
             {Data.profile[`${data === 0 ? "english" : "hindi"}`]}
           </div>
           <div className="Clickbtn">
